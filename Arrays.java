ee9ae7648c5ce6b36e9ad8db054b68973c406b14
Abhijit Kumbhar <120697855+abhijit-kumbhar@users.noreply.github.com>
2024-01-08 23:17:07 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -27,3 +27,22 @@ class Solution {
         return output;
     }
 }
+class Solution{
+    public int[] intersect(int[] nums1, int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> arr = new ArraysList<Integer>();
+        int i = 0, j = 0;
+        while(i<nums1.length && j,nums2.length){
+            if(nums1[i]>nums2[j]){
+                i++;
+            }
+            else if(nums[i]>nums[j]){
+                j++;
+            }
+            else{
+                
+            }
+        }
+    }
+}
