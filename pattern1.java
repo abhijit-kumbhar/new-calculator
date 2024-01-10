ef653b66a6bbc52e2fa88721b3b7229b1b9127bf
Abhijit Kumbhar <120697855+abhijit-kumbhar@users.noreply.github.com>
2024-01-10 21:33:38 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,1 +1,19 @@
+public class pattern1{
+  public static void printPattern(int n){
+    int i,j;
+    for(i=1;i<=n;i++){
+      for(j=1;j<=i;j++){
+        System.out.print("*");
+      }
+      for(j=1;j<=n-1;j++){
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
 
+  public static void mai(String args[]){
+    int n=6;
+    printPattern(n);
+  }
+}
