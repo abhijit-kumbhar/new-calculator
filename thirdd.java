8e33decfde922f542752dff3f3e53c5f0847e2e9
Abhijit Kumbhar <120697855+abhijit-kumbhar@users.noreply.github.com>
2024-01-11 23:07:06 +0530

    Update thirdd.java

--- thirdd.java
+++ thirdd.java
@@ -1,1 +1,18 @@
-
+public class thirdd{
+  public static main printPattern(int n){
+    int i,j;
+    for(i=1; i<=n; i++){
+      for(j=1;j<=n-1; j++){
+        System.out.print("*");
+      }
+      for(j=1;j<=i;j++){
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  } 
+  pubic static void main(String args[]){
+    int n=6;
+    printPattern(n);
+  }
+}
